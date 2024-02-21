1abdb71ae6ac92589fab67e71dbad1b2301e8a53
dogucoding <80362520+dogutesting@users.noreply.github.com>
2024-02-21 17:56:18 +0300

    14.155 - İlerleme Günlüğü: Çerez Sayfası
    + Çerez kutusu eklendi, boyutlandırmaları yapıldı.
    .. Çerez sayfası düzenlenmesi devam ediyor.
    ... cookiepoliciy_status cookie'si eklenecek ve userState durumu bu cookie'ye göre ayarlanacak, eğer kullanıcı reddettiyse yüklenmeyecek.

--- components/Main.js
+++ components/Main.js
@@ -9,6 +9,32 @@ export default function Main({children}) {
   const { nightMode, setNightMode, cookie_policy_div, setCookiePolicyDiv } = useAppContext();
 
   const [cerez_yonetim_sayfasi, setCerezYonetimSayfasi] = useState(false);
+  
+  /* const [checkbox_1, setCheckBox1] = useState(true);
+  const [checkbox_2, setCheckBox2] = useState(true);
+  const [checkbox_3, setCheckBox3] = useState(true);
+  const [checkbox_4, setCheckBox4] = useState(true); */
+
+  const [checked_map, setCheckedMap] = useState(new Map([
+                                                          ["checkbox_1", true], 
+                                                          ["checkbox_2", true],
+                                                          ["checkbox_3", true]
+                                                        ]));
+
+  const updateMap = (k, v) => {
+    setCheckedMap(new Map(checked_map.set(k, v)));
+  }
+
+  const accept_selected_cookie = () => {
+    console.log(checked_map);
+  }
+
+  const accept_all_cookie = () => {
+    
+    setCerezYonetimSayfasi(false);
+    //! Accept all cookie
+
+  }
 
   return (
     <> 
@@ -16,50 +42,50 @@ export default function Main({children}) {
           cookie_policy_div === true && (
             <div id='cookie_div'>
               <h1>Web Sitemiz Çerez Kullanıyor</h1>
-              <div>
-                <p>Tanımlama bilgilerini; sitemizin doğru şekilde çalışmasını sağlamak, içerikleri ve reklamları kişiselleştirmek ve site trafiğimizi analiz etmek için kullanıyoruz. Çerezler hakkında daha fazla bilgi için <Link href={"/cerez-politikasi"}>Çerez Politikası</Link> sayfamızı ziyaret edebilirsiniz. 'Kabul Et' seçeneğiyle Çerez Politikasını kabul etmiş sayılırsınız ve bu uyarı bir yıl boyunca <b>tekrar gösterilmez.</b></p>
-              </div>
+              <p>Tanımlama bilgilerini; sitemizin doğru şekilde çalışmasını sağlamak, içerikleri ve reklamları kişiselleştirmek ve site trafiğimizi analiz etmek için kullanıyoruz. Çerezler hakkında daha fazla bilgi için <Link href={"/cerez-politikasi"}>Çerez Politikası</Link> sayfamızı ziyaret edebilirsiniz. 'Kabul Et' seçeneğiyle Çerez Politikasını hızlıca kabul edebilirsiniz ve bu uyarı bir yıl boyunca <b>tekrar gösterilmez.</b></p>
               <div id='cookie_buttons'>
                 <button onClick={() => { setCerezYonetimSayfasi(true); setCookiePolicyDiv(false)}}>Çerez Yönetimi</button>
-                <button>Tüm Çerezleri Kabul Et</button>
+                <button onClick={() => {setCookiePolicyDiv(false)}}>Tüm Çerezleri Kabul Et</button>
               </div>
             </div>
           )
         }
         {
           cerez_yonetim_sayfasi && (
-            <div id='custom_cookie'>
-              <div id='cc_top'>
-                <h1>Çerez Yönetimi</h1>
-                <p>İzin vermek istediğiniz çerezleri seçin ve onaylayın.</p>
-              </div>
-
-              <div id='cc_center'>
-                <div>
-                  <input type="checkbox" checked="true"/>
-                  <span>İd çerezi</span>
-                  <p>Sadece 1 yıllığına tarayıcınızda tutulan çerez türüdür. Okuduğunuz makalelere benzer makaleleri önermek için kullanılır.</p>
+            <div id='custom_cookie_container'>
+              <div id='custom_cookie'>
+                <div id='cc_top'>
+                  <h1>Çerez Yönetimi</h1>
+                  <p>İzin vermek istediğiniz çerezleri seçin ve onaylayın.</p>
                 </div>
-                <div>
-                  <input type="checkbox" checked="true"/>
-                  <span>Ci Çerezi</span>
-                  <p>Sadece oturum süresince tarayıcınızda tutulan çerez türüdür. Sayfa kapatıldığında bu çerez yok edilir. Şehrinizde popüler makaleleri göstermek için IP üzerinden bulunduğunuz şehir tahmin edilir. Her zaman doğru sonuç vermez ve sadece bulunduğunuz şehri tahmin edebilir.</p>
+                <div id='cc_bottom'>
+                  <button onClick={accept_selected_cookie}>İşaretli Çerezleri Kabul Et</button>
+                  <button onClick={accept_all_cookie}>Tüm Çerezleri Kabul Et</button>
                 </div>
-                <div>
-                  <input type="checkbox" checked="true"/>
-                  <span>Kişiselleştirilmiş Reklamlar</span>
-                  <p>İlginize uygun reklamları göstermek için tarayıcıya çerezler tanımlar ve çerezleri 3. taraf ile paylaşabilir. (Örneğin: Google Reklam Ağı)</p>
+                <div id='cc_center'>
+                  <hr></hr>
+                  <div>
+                    <input type="checkbox" name='checkbox_1' onChange={(e) => updateMap(e.target.name, e.target.checked)} defaultChecked/>
+                    <span> ID ve CI çerezi</span>
+                    <p>ID ve CI çerezi tarayıcınızda tutulur, sevebileceğiniz makaleleri önermek için faydalıdır.
+                       ID çerezi 1 yıl boyunca tarayıcınızda saklanır ve süre sonunda yok edilir.
+                        Okuduğunuz makalelere benzer makaleleri önermek için kullanılır.
+                         CI çerezi sadece oturum süresince tarayıcınızda tutulur.
+                          Sayfa kapandığında bu çerez anında yok edilir.
+                           Şehrinizde popüler makaleleri göstermek için IP üzerinden bulunduğunuz şehir tahmin edilir.
+                            Her zaman doğru sonuç vermez ve sadece bulunduğunuz şehri tahmin edebilir.</p>
+                  </div>
+                  <div>
+                    <input type="checkbox" name='checkbox_2' onChange={(e) => updateMap(e.target.name, e.target.checked)} defaultChecked/>
+                    <span> Kişiselleştirilmiş Reklamlar</span>
+                    <p>İlginize uygun reklamları göstermek için tarayıcıya çerezler tanımlar ve çerezleri 3. taraf ile paylaşabilir. (Örneğin: Google Reklam Ağı)</p>
+                  </div>
+                  <div>
+                    <input type="checkbox" name='checkbox_3' onChange={(e) => updateMap(e.target.name, e.target.checked)} defaultChecked/>
+                    <span> Site Trafiği</span>
+                    <p>Site trafiğini hesaplamak ve diğer detayları göstermek için kullanılan Google Analytics'in tanımladığı çerezler.</p>
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" checked="true"/>
-                  <span>Site Trafiği</span>
-                  <p>Site trafiğini hesaplamak ve diğer detayları göstermek için kullanılan Google Analytics'in tanımladığı çerezler.</p>
-                </div>
-              </div>
-              
-              <div id='cc_bottom'>
-                <button>İşaretlediğim Çerezleri Kabul Et</button>
-                <button>Tüm Çerezleri Kabul Et</button>
               </div>
             </div>
           )
