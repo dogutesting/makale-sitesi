515cbe771e28000b4126bdb2cf0cc7908438ce68
dogucoding <80362520+dogutesting@users.noreply.github.com>
2024-03-11 12:08:23 +0300

    14.159 - Server Test
    + Google kütüphaneleri eklendi.
    ... Server'a yüklenip test edilmesi için commit.

--- middleware.js
+++ middleware.js
@@ -1,47 +1,6 @@
 import { NextResponse } from 'next/server';
-import { LRUCache } from 'lru-cache';
-import CONSTS from '@/consts.json';
 
-const click_server_name = "http://localhost:3000";
-
-/* const MAX_TIMEOUT = 4 * 60 * 60 * 1000; //* 4 saat olarak ayarlandı
-const ipLimits = new LRUCache(CONSTS); */
-
-// GET IP -> LOOK IP REQUEST SIZE -> RETURN ADD OR NOT ADD
-
-export default function middleware(request) {
-  const response = NextResponse.next();
-  if(request.method === "GET") {
-    if(request.nextUrl.pathname !== "/" && request.nextUrl.pathname !== "/404") {
-
-      /*
-      const pathname_1 = request.nextUrl.pathname.substring(1);
-      const id = request.cookies.get("id")?.value;
-      const ci = request.cookies.get("ci")?.value;
-      */
-
-      /* const ip_address = request.ip === undefined ? request.headers['x-forwarded-for'] : request.ip; */
-      /*
-      const ip_addres = (() => {
-        if(request.ip === undefined || request.ip === "") {
-            return request.headers['x-forwarded-for'];
-                                   'x-forwarded-for'
-        }
-        return request.ip;
-      })();
-      console.log(request.headers['x-real-ip']);
-      */
-      
-    }
-  }
-
-  return response;
-}
-
-export const config = {
-  // Tek 1 istek atması için düzenleme.
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
-}
+const topLevelUrl = "http://localhost:3000";
 
 const getDateAndTime = () => {
   const now = new Date();
@@ -55,33 +14,46 @@ const getDateAndTime = () => {
   return tarihVeSaat;
 }
 
-const customFetch = (id, ci, pathname, main=false) => {
-    //console.log("pathname: ", pathname);
-    let requestUrl = click_server_name+"/c-event/middleware";
-    if(main) {
-      requestUrl = click_server_name+"/c-event/main";
-    }
-    //console.log("middleware_fetch: ", requestUrl);
-    /* fetch(requestUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        req: "middleware",
-        auth: "Dm4i5dS",
-        body: {
-          user: {
-            id: id,
-            ci: ci  
-          },
-          status: {
-            pathname: pathname,
-            date: getDateAndTime()
-          }
+const customFetch = (id, ci, pathname) => {
+  fetch(topLevelUrl+"/api/userKey", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      req: "waypoint",
+      "type": type,
+      "data": {
+        user: {
+          "id": id,
+          "ci": ci
+        },
+        status: {
+          "pathname": pathname,
+          "date": getDateAndTime()
         }
-      })
-    }).catch(error => {
-      //
-    })  */
+      }
+    })
+  }).catch(error => {
+    //
+  });
+}
+
+export default function middleware(request) {
+  const response = NextResponse.next();
+  if(request.method === "GET") {
+    if(request.nextUrl.pathname !== "/" && request.nextUrl.pathname !== "/404") {
+      const pathname_1 = request.nextUrl.pathname.substring(1);
+      let id = request.cookies.get("id")?.value || "cookie-id";
+      let ci = request.cookies.get("ci")?.value || "cookie-ci";
+      customFetch(id, ci, pathname_1)
+    }
+  }
+
+  return response;
+}
+
+export const config = {
+  // Tek 1 istek atması için düzenleme.
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 }
