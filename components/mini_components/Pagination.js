facfb65270b360645fa19f516b6c99cb36ba8635
Doğukan <dgknsynnn@gmail.com>
2024-08-18 16:41:43 +0300

    16.21 - Pagination Değiştirildi
    + Pagination artık taşmaya başladığı için 3 butonlu pagination'a geçildi.

--- components/mini_components/Pagination.js
+++ components/mini_components/Pagination.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {useState, useEffect} from 'react';
 
 export default function Pagination({max, active, setActive, category}) {
 
@@ -19,20 +20,106 @@ export default function Pagination({max, active, setActive, category}) {
     }
   }
 
+  const[renderThis, setRenderThis] = useState();
+
+  function createPagination(active2, pageNumbers) {
+    const active = Number(active2);
+    if(active == 1) {
+      setRenderThis(<>
+      <Link
+          title={"Sayfa "+ active}
+          className={`page ${active == active ? 'active' : ''}`}
+          onClick={() => setActive(1)}
+          href={createPath(1)}
+      >
+          {active}
+      </Link>
+      <Link
+          title={"Sayfa "+ (active + 1)}
+          className={`page ${active == (active + 1) ? 'active' : ''}`}
+          onClick={() => setActive((active + 1))}
+          href={createPath((active + 1))}
+      >
+          {(active + 1)}
+      </Link>
+      <Link
+          title={"Sayfa " + (active + 2)}
+          className={`page ${active == (active + 2) ? 'active' : ''}`}
+          onClick={() => setActive((active + 2))}
+          href={createPath((active + 2))}
+      >
+          {(active + 2)}
+      </Link>
+      </>);
+    }
+  
+    else if(active == pageNumbers.length) {
+    setRenderThis(<>
+      <Link
+          title={"Sayfa "+ (active - 2)}
+          className={`page ${active == (active - 2) ? 'active' : ''}`}
+          onClick={() => setActive((active - 2))}
+          href={createPath((active - 2))}
+      >
+          {(active - 2)}
+      </Link>
+      <Link
+          title={"Sayfa "+ (active - 1)}
+          className={`page ${active == (active - 1) ? 'active' : ''}`}
+          onClick={() => setActive((active - 1))}
+          href={createPath((active - 1))}
+      >
+          {(active - 1)}
+      </Link>
+      <Link
+          title={"Sayfa " + active}
+          className={`page ${active == active ? 'active' : ''}`}
+          onClick={() => setActive(active)}
+          href={createPath(active)}
+      >
+          {active}
+      </Link>
+      </>);
+    }
+
+    else {
+      setRenderThis(<>
+        <Link
+            title={"Sayfa "+ (active - 1)}
+            className={`page ${active == (active - 1) ? 'active' : ''}`}
+            onClick={() => setActive((active - 1))}
+            href={createPath((active - 1))}
+        >
+            {active - 1}
+        </Link>
+        <Link
+            title={"Sayfa "+ active}
+            className={`page ${active == active ? 'active' : ''}`}
+            onClick={() => setActive(active)}
+            href={createPath(active)}
+        >
+            {active}
+        </Link>
+        <Link
+            title={"Sayfa " + (active + 1)}
+            className={`page ${active == (active + 1) ? 'active' : ''}`}
+            onClick={() => setActive((active + 1))}
+            href={createPath((active + 1))}
+        >
+            {(active + 1)}
+        </Link>
+        </>);
+    }
+  }
+
+  useEffect(() => {
+    createPagination(active, pageNumbers);
+  }, [active])
+
   return (
     <div className="pagination-container">
         <div className="pagination">
-            {pageNumbers.map((pageNumber) => (
-            <Link
-                title={"Sayfa "+pageNumber}
-                key={pageNumber}
-                className={`page ${active == pageNumber ? 'active' : ''}`}
-                onClick={() => setActive(pageNumber)}
-                href={createPath(pageNumber)}
-            >
-                {pageNumber}
-            </Link>
-            ))}
+            {renderThis}
         </div>
     </div>
   )
